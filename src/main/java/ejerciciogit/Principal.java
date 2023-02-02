a0a4c7bccc36d45a238f5046e5682b4e31af06dd
dam <dam@203P15>
2023-02-02 08:45:31 +0100

    Líneas en el método main.

---
+++ src/main/java/ejerciciogit/Principal.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejerciciogit;
+
+/**
+ *
+ * @author dam
+ */
+public class Principal {
+    public static void main(String[] args) {
+        System.out.println("EjercicioGit");
+        System.out.println("Control de versiones");
+    }
+    
+}
